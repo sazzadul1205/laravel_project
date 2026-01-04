4814a64cb3b27b2339b26c8014ef088147b1ddcd
Sazzadul <psazzadul@gmail.com>
2026-01-04 13:05:49 +0600

    Broke downb the Default and amade a Good Layout

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,5 +1,40 @@
 @extends('backend.layout.default')
 
+@section('head')
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Biz Admin - Multipurpose bootstrap 4 admin templates</title>
+        <!-- Tell the browser to be responsive to screen width -->
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description"
+            content="Biz Admin is a Multipurpose bootstrap 4 Based Dashboard & Admin Site Responsive Template by uxliner." />
+        <meta name="keywords"
+            content="admin, admin dashboard, admin template, cms, crm, Biz Admin, Biz Adminadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
+        <meta name="author" content="uxliner" />
+        <!-- v4.1.3 -->
+        <link rel="stylesheet" href="dist/bootstrap/css/bootstrap.min.css">
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" sizes="16x16" href="dist/img/favicon-16x16.png">
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+        <!-- Theme style -->
+        <link rel="stylesheet" href="dist/css/style.css">
+        <link rel="stylesheet" href="dist/css/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="dist/css/et-line-font/et-line-font.css">
+        <link rel="stylesheet" href="dist/css/themify-icons/themify-icons.css">
+        <link rel="stylesheet" href="dist/css/simple-lineicon/simple-line-icons.css">
+        <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+
+
+    </head>
+@endsection
+
 
 @section('content')
     <div class="content-wrapper">
@@ -58,224 +93,31 @@
                     </div>
                 </div>
             </div>
-            <!-- /.row -->
 
-            <div class="row">
-                <div class="col-lg-8">
-                    <div class="info-box">
-                        <div class="col-12">
-                            <h5>Revenue Statistics</h5>
-                            <div class="row m-t-2 m-b-2">
-                                <div class="col-md-6">
-                                    <h1 class="font-weight-500">$23,743</h1>
-                                    <p>Total Revenue</p>
-                                </div>
-                                <div class="col-md-3">
-                                    <h6 class="text-blue font-weight-bold">Organic Traffic</h6>
-                                    <p class="f-13">+ 40% this month</p>
-                                </div>
-                                <div class="col-md-3">
-                                    <h6 class="text-green font-weight-bold">Page Views</h6>
-                                    <p class="f-13">+ 25% this month</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="earning"></div>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="card m-b-3">
-                        <div class="card-body">
-                            <div class="m-b-3 font-weight-bold">
-                                <h5>New Clients
-                                    <button type="button" class="btn btn-sm btn-rounded btn-info pull-right">250</button>
-                                </h5>
-                            </div>
-                            <div class="m-b-2 f-25">09.5% <i class="ti-bar-chart pull-right f-30"></i> </div>
-                            <div><i class="f-20 ti-upload text-aqua"></i> 35% Increase in 30 Days</div>
-                        </div>
-                    </div>
-                    <div class="card bg-info m-b-3">
-                        <div class="card-body text-white">
-                            <div class="m-b-1 font-weight-500">
-                                <h5><span class="text-white">This Year Income</span>
-                                    <button type="button" class="btn btn-sm btn-rounded btn-danger pull-right">View
-                                        more</button>
-                                </h5>
-                            </div>
-                            <div class="f-40 font-weight-500">$58,869</div>
-                            <div class="m-t-1"><span id="spa-pie-1" class="m-t-3 pull-right"></span> <span
-                                    class="f-13">New Orders</span>
-                                <h3>$32,535</h3>
-                                <span class="f-13">Online Revenue</span>
-                                <h3>$ 26,334</h3>
-                                <span class="f-13">Total Profit</span>
-                                <h3>$ 58,869</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.row -->
-
-            <div class="row">
-                <div class="col-lg-5 m-b-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5>Recent Activities <span class="pull-right f-13"><a href="#">View
-                                        All</a></span></h5>
-                            <div class="message-widget"> <a href="#">
-                                    <div class="user-img pull-left"> <img src="dist/img/img1.jpg"
-                                            class="img-circle img-responsive" alt="User Image"> <span
-                                            class="profile-status online pull-right"></span> </div>
-                                    <div class="mail-contnet">
-                                        <h5>Florence Douglas</h5>
-                                        <span class="mail-desc">Lorem Ipsum is simply dummy text of the printing
-                                            and type setting industry. Lorem Ipsum has been.</span> <span
-                                            class="time">9:30 AM</span>
-                                    </div>
-                                </a> <a href="#">
-                                    <div class="user-img pull-left"> <img src="dist/img/img3.jpg"
-                                            class="img-circle img-responsive" alt="User Image"> <span
-                                            class="profile-status invisable pull-right"></span> </div>
-                                    <div class="mail-contnet">
-                                        <h5>Florence Douglas</h5>
-                                        <span class="mail-desc">Lorem Ipsum is simply dummy text of the printing
-                                            and type setting industry. Lorem Ipsum has been.</span> <span
-                                            class="time">10:30 AM</span>
-                                    </div>
-                                </a> <a href="#">
-                                    <div class="user-img pull-left"> <img src="dist/img/img4.jpg"
-                                            class="img-circle img-responsive" alt="User Image"> <span
-                                            class="profile-status offline pull-right"></span> </div>
-                                    <div class="mail-contnet">
-                                        <h5>Florence Douglas</h5>
-                                        <span class="mail-desc">Lorem Ipsum is simply dummy text of the printing
-                                            and type setting industry. Lorem Ipsum has been.</span> <span
-                                            class="time">12:30 AM</span>
-                                    </div>
-                                </a> <a href="#">
-                                    <div class="user-img pull-left"> <img src="dist/img/img3.jpg"
-                                            class="img-circle img-responsive" alt="User Image"> <span
-                                            class="profile-status invisable pull-right"></span> </div>
-                                    <div class="mail-contnet">
-                                        <h5>Florence Douglas</h5>
-                                        <span class="mail-desc">Lorem Ipsum is simply dummy text of the printing
-                                            and type setting industry. Lorem Ipsum has been.</span> <span
-                                            class="time">10:30 AM</span>
-                                    </div>
-                                </a> <a href="#">
-                                    <div class="user-img pull-left"> <img src="dist/img/img1.jpg"
-                                            class="img-circle img-responsive" alt="User Image"> <span
-                                            class="profile-status online pull-right"></span> </div>
-                                    <div class="mail-contnet">
-                                        <h5>Florence Douglas</h5>
-                                        <span class="mail-desc">Lorem Ipsum is simply dummy text of the printing
-                                            and type setting industry. Lorem Ipsum has been.</span> <span
-                                            class="time">9:30 AM</span>
-                                    </div>
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5>Recent Chats <span class="pull-right f-13"><a href="#">View All</a></span>
-                            </h5>
-                            <div class="box-body">
-                                <!-- Conversations are loaded here -->
-                                <div class="direct-chat-messages" style="height:377px;">
-                                    <div class="direct-chat-msg">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-left">Alexander Pierce</span> <span
-                                                class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img2.jpg" alt="user image">
-                                        <div class="direct-chat-text"> A small river named Duden flows by their
-                                            place and supplies it with the necessary. </div>
-                                    </div>
-                                    <div class="direct-chat-msg right">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-right">Sarah Bullock</span> <span
-                                                class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img3.jpg" alt="user image">
-                                        <!-- /.direct-chat-img -->
-                                        <div class="direct-chat-text"> You better believe it! </div>
-                                    </div>
-                                    <div class="direct-chat-msg">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-left">Alexander Pierce</span> <span
-                                                class="direct-chat-timestamp pull-right">23 Jan 5:37 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img4.jpg" alt="user image">
-                                        <div class="direct-chat-text"> A small river named Duden flows by their
-                                            place and supplies it with the necessary. </div>
-                                    </div>
-                                    <div class="direct-chat-msg right">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-right">Sarah Bullock</span> <span
-                                                class="direct-chat-timestamp pull-left">23 Jan 6:10 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img5.jpg" alt="user image">
-                                        <div class="direct-chat-text"> I would love to. </div>
-                                    </div>
-                                    <div class="direct-chat-msg">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-left">Alexander Pierce</span> <span
-                                                class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img6.jpg" alt="user image">
-                                        <div class="direct-chat-text"> A small river named Duden flows by their
-                                            place and supplies it with the necessary. </div>
-                                    </div>
-                                    <div class="direct-chat-msg right">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-right">Sarah Bullock</span> <span
-                                                class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img3.jpg" alt="user image">
-                                        <!-- /.direct-chat-img -->
-                                        <div class="direct-chat-text"> You better believe it! </div>
-                                    </div>
-                                    <div class="direct-chat-msg">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-left">Alexander Pierce</span> <span
-                                                class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img6.jpg" alt="user image">
-                                        <div class="direct-chat-text"> A small river named Duden flows by their
-                                            place and supplies it with the necessary. </div>
-                                    </div>
-                                    <div class="direct-chat-msg right">
-                                        <div class="direct-chat-info clearfix"> <span
-                                                class="direct-chat-name pull-right">Sarah Bullock</span> <span
-                                                class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-                                        </div>
-                                        <img class="direct-chat-img" src="dist/img/img3.jpg" alt="user image">
-                                        <!-- /.direct-chat-img -->
-                                        <div class="direct-chat-text"> You better believe it! </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="box-footer">
-                                <form action="#" method="post">
-                                    <div class="input-group">
-                                        <input name="message" placeholder="Type Message ..." class="form-control"
-                                            type="text">
-                                        <span class="input-group-btn">
-                                            <button type="button" class="btn btn-warning btn-flat">Send</button>
-                                        </span>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.row -->
         </div>
-        <!-- /.content -->
+
+
+        <div class="text-center font-bold">
+            <h4>Welcome New User</h4>
+        </div>
+
     </div>
+@endsection
+
+@section('scripts')
+    <!-- jQuery 3 -->
+    <script src="dist/js/jquery.min.js"></script>
+    <script src="dist/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- template -->
+    <script src="dist/js/bizadmin.js"></script>
+
+    <!-- JQuery Spark lines -->
+    <script src="dist/plugins/jquery-sparklines/sparkline-int.js"></script>
+    <script src="dist/plugins/jquery-sparklines/jquery.sparkline.min.js"></script>
+
+    <!-- Morris JavaScript -->
+    <script src="dist/plugins/morris/morris.js"></script>
+    <script src="dist/plugins/raphael/raphael-min.js"></script>
+    <script src="dist/plugins/functions/dashboard1.js"></script>
 @endsection
--- resources/views/backend/layout/default.blade.php
+++ resources/views/backend/layout/default.blade.php
@@ -1,71 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Biz Admin - Multipurpose bootstrap 4 admin templates</title>
-
-    {{-- Meta --}}
-    <meta name="author" content="uxliner" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description"
-        content="Biz Admin is a Multipurpose bootstrap 4 Based Dashboard & Admin Site Responsive Template by uxliner." />
-    <meta name="keywords"
-        content="admin, admin dashboard, admin template, cms, crm, Biz Admin, Biz Adminadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
+{{-- Head --}}
+@yield('head')
 
 
-    <!-- v4.1.3 -->
-    <link rel="stylesheet" href="dist/bootstrap/css/bootstrap.min.css">
+<body class="skin-blue sidebar-mini">
+    <div class="wrapper boxed-wrapper">
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="dist/img/favicon-16x16.png">
+        {{-- Nav --}}
+        @include('backend.layout.nav')
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+        {{-- Sidebar --}}
+        @include('backend.layout.sidebar')
 
-    <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/style.css">
-    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-    <link rel="stylesheet" href="dist/css/et-line-font/et-line-font.css">
-    <link rel="stylesheet" href="dist/css/themify-icons/themify-icons.css">
-    <link rel="stylesheet" href="dist/css/simple-lineicon/simple-line-icons.css">
-    <link rel="stylesheet" href="dist/css/font-awesome/css/font-awesome.min.css">
+        {{-- Content --}}
+        @yield('content')
 
-</head>
+        {{-- footer --}}
+        @include('backend.layout.footer')
 
+        {{-- Control Sidebar --}}
+        @include('backend.layout.controlSidebar')
+    </div>
 
-<body>
-
-    {{-- Nav --}}
-    @include('backend.layout.nav')
-
-    {{-- Sidebar --}}
-    @include('backend.layout.sidebar')
-
-    {{-- Content --}}
-    @yield('content')
-
-    {{-- footer --}}
-    @include('backend.layout.footer')
-
-    {{-- Control Sidebar --}}
-    @include('backend.layout.controlSidebar')
-
-    <!-- jQuery 3 -->
-    <script src="dist/js/jquery.min.js"></script>
-    <script src="dist/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- template -->
-    <script src="dist/js/bizadmin.js"></script>
-
-    <!-- JQuery Spark lines -->
-    <script src="dist/plugins/jquery-sparklines/sparkline-int.js"></script>
-    <script src="dist/plugins/jquery-sparklines/jquery.sparkline.min.js"></script>
-
-    <!-- Morris JavaScript -->
-    <script src="dist/plugins/morris/morris.js"></script>
-    <script src="dist/plugins/raphael/raphael-min.js"></script>
-    <script src="dist/plugins/functions/dashboard1.js"></script>
+    {{-- Scripts --}}
+    @yield('scripts')
 
 </body>
